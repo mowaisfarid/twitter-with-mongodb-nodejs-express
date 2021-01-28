9bfb86b16fad9f5bbe781d28fd4245f269cadaba
Muhammad zubair <mowaisfarid@gmail.com>
2021-01-28 15:46:10 +0500

    fgfgf

--- dbrepo/models.js
+++ dbrepo/models.js
@@ -5,8 +5,8 @@
 
 var mongoose = require('mongoose')
 
-// let dbURI = DBURI;
-let dbURI = "mongodb+srv://owais:owais@cluster0.auftw.mongodb.net/tweeter-db?retryWrites=true&w=majority";
+let dbURI = DBURI;
+// let dbURI = "mongodb+srv://owais:owais@cluster0.auftw.mongodb.net/tweeter-db?retryWrites=true&w=majority";
 
 mongoose.connect(dbURI, { useNewUrlParser: true, useUnifiedTopology: true });
 
