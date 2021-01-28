6113dde455a3c97328c5868d80e5a7e2aa4fe6f9
Muhammad Owais Farid <61145774+mowaisfarid@users.noreply.github.com>
2021-01-28 16:33:35 +0500

    core work

--- core/index.js
+++ core/index.js
@@ -1,7 +1,8 @@
 // var DBURI =
 
 module.exports = {
-    // DBURI: process.env.DBURI || "mongodb+srv://owais:owais@cluster0.auftw.mongodb.net/tweeter-db?retryWrites=true&w=majority",
+//     DBURI: process.env.DBURI || "mongodb+srv://owais:owais@cluster0.auftw.mongodb.net/tweeter-db?retryWrites=true&w=majority",
+    DBURI: process.env.DBURI ,
     SERVER_SECRET: process.env.SECRET || "1234",
     // CLIENT_SEC: process.env.CLIENT_SEC || "abc"
 
